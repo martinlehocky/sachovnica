a06bb1b096750319e515a1d13fcd0133bb6c2c18
martinlehocky <martin.lehocky2007@gmail.com>
2026-09-21 16:47:09 +0200

    Generovanie policok sachovnice pomocou TypeScriptu

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const board = document.getElementById("chessboard")!;
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        board.append(square);
+    }
+}
