16ffa30b0254f10b1a904a592b7bad25efd70a8c
martinlehocky <martin.lehocky2007@gmail.com>
2026-09-21 17:06:24 +0200

    Pridanie oznacenia stlpcov A-H a riadkov 1-8

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,5 @@
 const board = document.getElementById("chessboard");
+const letters = "ABCDEFGH";
 for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
         const square = document.createElement("div");
@@ -8,6 +9,18 @@ for (let row = 0; row < 8; row++) {
         }
         else {
             square.classList.add("dark");
+        }
+        if (col === 0) {
+            const number = document.createElement("span");
+            number.classList.add("label", "number");
+            number.textContent = String(8 - row);
+            square.append(number);
+        }
+        if (row === 7) {
+            const letter = document.createElement("span");
+            letter.classList.add("label", "letter");
+            letter.textContent = letters[col];
+            square.append(letter);
         }
         board.append(square);
     }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 const board = document.getElementById("chessboard")!;
+const letters = "ABCDEFGH";
 
 for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
@@ -9,6 +10,20 @@ for (let row = 0; row < 8; row++) {
             square.classList.add("light");
         } else {
             square.classList.add("dark");
+        }
+
+        if (col === 0) {
+            const number = document.createElement("span");
+            number.classList.add("label", "number");
+            number.textContent = String(8 - row);
+            square.append(number);
+        }
+
+        if (row === 7) {
+            const letter = document.createElement("span");
+            letter.classList.add("label", "letter");
+            letter.textContent = letters[col];
+            square.append(letter);
         }
 
         board.append(square);
