33e800c684ba36a82feafed2cc05125b9fe2ec25
martinlehocky <martin.lehocky2007@gmail.com>
2026-09-21 16:58:41 +0200

    Pridanie striedavych farieb a stylovania sachovnice

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,12 @@ for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        if ((row + col) % 2 === 0) {
+            square.classList.add("light");
+        }
+        else {
+            square.classList.add("dark");
+        }
         board.append(square);
     }
 }
